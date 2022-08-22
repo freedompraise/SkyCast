5755adf098551a1e3264b08825dddafb177b0489
freedompraise <dikepraise119@gmail.com>
2022-08-22 01:04:15 +0100

    notNullCconstraint attacked

--- src/views.py
+++ src/views.py
@@ -3,35 +3,39 @@ from django.shortcuts import render, redirect
 import requests
 from .models import City
 from django.contrib import messages 
+from datetime import datetime, date
+
 # Create your views here.
 
 def base(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=816582def5ad0a83096393ac18cf1419'
+    page = 'search'
+    if request.method == 'POST':
+        page= 'display'
     city = request.POST.get('city')
     city_weather = requests.get(url.format(city)).json()
-    if city_weather['cod'] != '404':  # conditional when the city queried was found
+    if city_weather['cod'] == '404':  # conditional when the city queried was found
+        return redirect('404')
         
-        if city is not None:
-            City.objects.update_or_create(
-                name = city,
-                temp= (city_weather['main']['temp']),
-                max = city_weather['main']['temp_max'],
-                min = city_weather['main']['temp_min'],
-         )
-        
-        context = {
-        'city':city,
-        'cities':City.objects.all(),
-        'temperature':city_weather['main']['temp'],
+       
+    if city is not None:
+        City.objects.get_or_create(
+            name = city,
+            temp= (city_weather['main']['temp']),
+            max = city_weather['main']['temp_max'],
+            min = city_weather['main']['temp_min'],
+        )
+    context = {
+        'city':City.objects.filter(name=city),
+        'cities':City.objects.all()[:4],
         'description':city_weather['weather'][0]['description'],
-        'icon':city_weather['weather'][0]['icon'],
-        'max':int(city_weather['main']['temp']) + 4,
-        'min':int(city_weather['main']['temp']) - 4,
-        'city_data':city_weather,
+        'feels_like': city_weather['main']['feels_like'],
+        'now':datetime.now().strftime("%c")
     }
-        return render(request,'src/home-view.html',context)
-    else:
-        return render(request,'src/404.html')
+    return render(request,'src/home-view.html',context)
 
 def loginPage(request):
     return render(request,'src/login.html')
+
+def pageNotFound(request):
+    return render(request,'src/404.html') 

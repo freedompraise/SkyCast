b44af4fe1a9ab4a4a0a3cddb1b37ad6011368cb2
Praise Freedom Dike <dikepraise119@gmail.com>
2023-02-17 15:02:57 +0100

    user field added to city model

--- src/models.py
+++ src/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class City(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=225)
     time = models.DateTimeField(auto_now_add=True)
     temp = models.DecimalField(max_digits=5, decimal_places=2,null=True)

cebd9104bda616a83695a358b04ffe4a955ef47b
Praise Freedom Dike <dikepraise119@gmail.com>
2023-02-27 20:52:41 +0100

    views code refactored

--- src/views.py
+++ src/views.py
@@ -1,59 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages 
 from django.conf import settings
-from django.contrib.auth import authenticate, logout 
-from django.contrib.auth import login as auth_login
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from .forms import CustomUserCreationForm
-
-
 from .models import City
 
 import requests
-from datetime import datetime, date
+from datetime import datetime
 
 import mailchimp_marketing as MailchimpMarketing
 
-
-api_key = settings.MAILCHIMP_API_KEY
-server = settings.MAILCHIMP_DATA_CENTER
-list_id = settings.MAILCHIMP_EMAIL_LIST_ID
-
-# Create your views here.
-
-def allCities(request):
-    context={
-        'cities':City.objects.all()
-    }
-    return render(request, 'src/results.html',context)
+MAILCHIMP_API_KEY = settings.MAILCHIMP_API_KEY
+MAILCHIMP_DATA_CENTER = settings.MAILCHIMP_DATA_CENTER
+MAILCHIMP_EMAIL_LIST_ID = settings.MAILCHIMP_EMAIL_LIST_ID
 
 
 @login_required(login_url='login')
 def base(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=816582def5ad0a83096393ac18cf1419'
-    #if request.method == 'POST':
-    city = request.POST.get('city') if request.POST.get('city') else 'Lagos'
+    city = request.POST.get('city', 'Lagos')
     city_weather = requests.get(url.format(city)).json()
-    # else:
-    #     return redirect('search')
-    
+
     if city_weather['cod'] == '404':  # conditional when the city queried was found
         return redirect('404')
         
-       
-    if city is not None:
-         if City.objects.filter(name = city.lower()).exists() == False :   #to avoid adding a city twice to the database
-            City.objects.get_or_create(  
-            user = request.user,    
-            name = city.lower(),
-            temp= 5/9* (city_weather['main']['temp']-32),
-            max = 5/9*(city_weather['main']['temp_max']-32),
-            min = 5/9*(city_weather['main']['temp_min']-32),
+    if City.objects.filter(name=city.lower()).exists() == False:   #to avoid adding a city twice to the database
+        City.objects.create(  
+            user=request.user,    
+            name=city.lower(),
+            temp=5/9*(city_weather['main']['temp']-32),
+            max=5/9*(city_weather['main']['temp_max']-32),
+            min=5/9*(city_weather['main']['temp_min']-32),
          )
     
-
     context = {
         'city':City.objects.get(name=city.lower() if city is not None else ''), 
         'cities':City.objects.order_by('-time')[:4], # gets the latest three citiess
@@ -63,16 +45,21 @@ def base(request):
         'now':datetime.now().strftime("%c"),
         'user':request.user
     }
-
    
     return render(request,'src/home-view.html',context)
 
 
-def pageNotFound(request):
+def all_cities(request):
+    cities = City.objects.all()
+    context = {'cities': cities}
+    return render(request, 'src/results.html', context)
+
+
+def page_not_found(request, exception):
     return render(request,'src/404.html') 
 
 
-def registerPage(request):
+def register(request):
     if request.method == 'POST':        
         email = request.POST.get('email')
         username = request.POST.get('username')
@@ -80,8 +67,7 @@ def registerPage(request):
         last_name = request.POST.get('last_name')
         password = request.POST.get('password')
         password1 = request.POST.get('password1')
-        user = authenticate(username=username, password=password)
-
+        
         if password == password1:
             user = User.objects.create_user(
                 username=username,
@@ -90,47 +76,45 @@ def registerPage(request):
                 first_name=first_name,
                 last_name=last_name,
             )
-            return redirect('home')
-        else:
-            error_msg = 'Passwords do not match'
-      
-        try:
-            # Initialize the Mailchimp SDK
-            client = MailchimpMarketing.Client()
-            client.set_config({
-                "api_key": api_key,
-                "server": server
-            })
 
             # Add the user to the Mailchimp contact list
-            response = client.lists.add_list_member(list_id, {
-                "email_address": email,
-                "status": "subscribed",
-                "merge_fields": {
-                "FNAME": first_name,
-                "LNAME": last_name,
-                "USERNAME": username
-                    }
+            try:
+                client = MailchimpMarketing.Client()
+                client.set_config({
+                    "api_key": MAILCHIMP_API_KEY,
+                    "server": MAILCHIMP_DATA_CENTER
                 })
+
+                response = client.lists.add_list_member(MAILCHIMP_EMAIL_LIST_ID, {
+                    "email_address": email,
+                    "status": "subscribed",
+                    "merge_fields": {
+                    "FNAME": first_name,
+                    "LNAME": last_name,
+                    "USERNAME": username
+                        }
+                    })
                 # Log the response
-            print(response)
+                print(response)
 
                  # If the subscriber was added successfully, log the user in and redirect to the home page
-            if user is not None:
-                auth_login(request, user)
-                return redirect('home')
+                if user is not None:
+                    auth_login(request, user)
+                    return redirect('home')
 
-        except Exception as e:
+            except Exception as e:
                 # Handle any API errors
                 print("Error: {}".format(e))
-                User.objects.get(username=request.user.username).delete()
+                user.delete()
+                messages.error(request, 'Something went wrong. Please try again later.')
+        else:
+            messages.error(request, 'Invalid form data. Please correct the errors below.')
     else:
         form = CustomUserCreationForm()
     return render(request,'src/register.html', {'form':form})
 
 
 def loginPage(request):
-    error_message = None
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -139,7 +123,6 @@ def loginPage(request):
             auth_login(request, user)
             return redirect('home')
         else:
-            error_message = 'Invalid username or password.'
-    return render(request, 'src/login.html', {'error_message': error_message})
-
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'src/login.html')
  

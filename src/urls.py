015b5b4348de6769ca91c79c18f6755b0f4f8731
Praise Freedom Dike <dikepraise119@gmail.com>
2023-02-27 20:55:22 +0100

    views adjusted to match urls

--- src/urls.py
+++ src/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.base,name='home'),
     path('auth/',views.loginPage, name='login'),
-    path('404/',views.pageNotFound, name = "404"),
-    path('results/', views.allCities, name = "cities"),
-    path('auth/register/', views.registerPage, name = 'register' ),
+    path('404/',views.page_not_found, name = "404"),
+    path('results/', views.all_cities, name = "cities"),
+    path('auth/register/', views.register, name = 'register' ),
 ]

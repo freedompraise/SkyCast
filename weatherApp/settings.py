960b9b31bfe509f822ab30350550b02bae32f20e
Praise Freedom Dike <dikepraise119@gmail.com>
2023-03-23 14:34:38 +0100

    resolve the rest warnings from ``manage.py check``

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -132,5 +132,9 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 7200
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True

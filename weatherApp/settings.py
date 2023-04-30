5280943001f0925d6a3503c890f357c26393a8f8
Praise Freedom Dike <dikepraise119@gmail.com>
2023-04-30 21:32:21 +0100

    configure settings file for production

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -6,11 +6,12 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
+APPEND_SLASH=False
 # Application definition
 
 INSTALLED_APPS = [
@@ -31,6 +32,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'weatherApp.urls'
@@ -42,7 +44,6 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -58,25 +59,14 @@ WSGI_APPLICATION = 'weatherApp.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
@@ -86,7 +76,6 @@ SESSION_COOKIE_AGE = 1800 # user session time of 30 mins
 MAILCHIMP_API_KEY = config('MAILCHIMP_API_KEY')
 MAILCHIMP_DATA_CENTER = config('MAILCHIMP_DATA_CENTER')
 MAILCHIMP_EMAIL_LIST_ID = config('MAILCHIMP_EMAIL_LIST_ID')
-SECRET_KEY = config('SECRET_KEY')
 OPEN_WEATHER_API_KEY = config('OPEN_WEATHER_API_KEY')
 
 # Internationalization
@@ -106,11 +95,32 @@ USE_TZ = True
 
 # define the location of your static files
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+MEDIA_ROOT=BASE_DIR/"static/images"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / 'static'
 ]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# set SECURE_HSTS_SECONDS to a non-zero value to enable HSTS
+SECURE_HSTS_SECONDS = 315300 # or any value that you prefer
+
+# set SECURE_SSL_REDIRECT to True to enforce SSL connection
+SECURE_SSL_REDIRECT = True
+
+# set SECURE_HSTS_INCLUDE_SUBDOMAINS to True if all subdomains should be served via SSL
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+# set SECURE_HSTS_PRELOAD to True to submit your site to the browser preload list
+SECURE_HSTS_PRELOAD = True
+
+# set SESSION_COOKIE_SECURE and CSRF_COOKIE_SECURE to True to use secure-only cookies
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECRET_KEY = get_random_secret_key()

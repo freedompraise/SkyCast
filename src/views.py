af266d806510c132e174207b1d5302b1ed74a8ef
Praise Freedom Dike <dikepraise119@gmail.com>
2023-06-02 15:07:27 +0100

    rename allCities views to all_cities

--- src/views.py
+++ src/views.py
@@ -23,19 +23,29 @@ url = settings.OPEN_WEATHER_API_KEY
 DEFAULT_CITY = 'Lagos' # default city to be displayed on the home page
 
 def create_city(city):
-    if city is not None:
-         if City.objects.filter(name = city.lower()).exists() == False :   #to avoid adding a city twice to the database
-            City.objects.get_or_create(  
-            user = request.user,    
-            name = city.lower(),
-            temp= 5/9* (city_weather['main']['temp']-32),
-            max = 5/9*(city_weather['main']['temp_max']-32),
-            min = 5/9*(city_weather['main']['temp_min']-32),
-         )
+    # add the city to the database is user is logged in
+    if request.user.is_authenticated:
+        if city is not None:
+             if City.objects.filter(name = city.lower()).exists() == False :   #to avoid adding a city twice to the database
+                City.objects.get_or_create(  
+                user = request.user,    
+                name = city.lower(),
+                temp= 5/9* (city_weather['main']['temp']-32),
+                max = 5/9*(city_weather['main']['temp_max']-32),
+                min = 5/9*(city_weather['main']['temp_min']-32),
+            )
+    else:
+        if city:
+            if 'queried_cities' not in request.session:
+                request.session['queried_cities'] = []  # Initialize an empty list if it doesn't exist yet
+            queried_cities = request.session['queried_cities']
+            queried_cities.append(city)
+            request.session.modified = True  # Save the session after modifying it
+
 
 
 @login_required(login_url="login")
-def allCities(request):
+def all_cities(request):
     context={
         'cities':City.objects.filter(user = request.user).order_by('-time')[:4],
     }

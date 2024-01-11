ef034d61be77ec824342ecb497af1fdd02f4f05f
Praise Freedom Dike <dikepraise119@gmail.com>
2024-01-11 13:20:57 +0100

    Remove user field from City model and add ClimateChange model

--- src/models.py
+++ src/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class City(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=225)
     time = models.DateTimeField(auto_now_add=True)
-    temp = models.DecimalField(max_digits=5, decimal_places=2,null=True)
-    max =  models.DecimalField(max_digits=5, decimal_places=2,null=True)
-    min =  models.DecimalField(max_digits=5, decimal_places=2,null = True)
-      # weather_info = models.CharField(max_length=225)
+    temp = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    max = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    min = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    # weather_info = models.CharField(max_length=225)
 
     class Meta:
         verbose_name_plural = "cities"
@@ -18,8 +18,9 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
+
 class ClimateChange(models.Model):
-  city = models.ForeignKey(City, on_delete=models.CASCADE)
-  time = models.DateField()
-  temp = models.FloatField()
-  # add more fields as needed
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    time = models.DateField()
+    temp = models.FloatField()
+    # add more fields as needed

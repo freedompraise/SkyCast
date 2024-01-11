e0651aa7a2863102c4e49d7f403b6ff3541c66c0
Praise Freedom Dike <dikepraise119@gmail.com>
2024-01-11 13:20:40 +0100

    refactor url

--- src/urls.py
+++ src/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
-from . import views, weatherViews
+from .views import (
+    base,
+    loginPage,
+    pageNotFound,
+    registerPage,
+    query_all_cities,
+)
 
 
 urlpatterns = [
-    path('',views.base,name='home'),
-    path('search/',views.search_city,name='search'),
-    path('auth/',views.loginPage, name='login'),
-    path('404/',views.pageNotFound, name = "404"),
-    path('results/', views.query_all_cities, name = "cities"),
-    path('auth/register/', views.registerPage, name = 'register' ),
-    # path('city/<int:city_id>/climate-change/', weatherViews.ClimateChangeView(), name = 'climate_change'),
+    path("", base, name="home"),
+    path("login/", loginPage, name="login"),
+    path("register/", registerPage, name="register"),
+    path("404/", pageNotFound, name="404"),
+    path("results/", query_all_cities, name="results"),
 ]

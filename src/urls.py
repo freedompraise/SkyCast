0b722eebef3dc2edcd566ce6ce9232eadb9c38fc
Praise Freedom Dike <dikepraise119@gmail.com>
2024-01-12 03:00:04 +0100

    Add search functionality to URLs

--- src/urls.py
+++ src/urls.py
@@ -3,6 +3,7 @@ from .views import (
     base,
     loginPage,
     pageNotFound,
+    search_city,
     registerPage,
     query_all_cities,
 )
@@ -11,6 +12,7 @@ from .views import (
 urlpatterns = [
     path("", base, name="home"),
     path("login/", loginPage, name="login"),
+    path("search/", search_city, name="search"),
     path("register/", registerPage, name="register"),
     path("404/", pageNotFound, name="404"),
     path("results/", query_all_cities, name="results"),
